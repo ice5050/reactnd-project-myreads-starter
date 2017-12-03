409405e409a1c5c397f47af84be4d84da4ed14d4
Ice5050 <ice50505050@hotmail.com>
2017-12-03 18:55:00 +0700

    BookShelf component

--- src/Book.js
+++ src/Book.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from "react";
 
 class Book extends React.Component {
   render() {
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: this.props.width, height: this.props.height, backgroundImage: `url("${this.props.backgroundImage}")` }}></div>
+          <div
+            className="book-cover"
+            style={{
+              width: this.props.width,
+              height: this.props.height,
+              backgroundImage: `url("${this.props.backgroundImage}")`
+            }}
+          />
           <div className="book-shelf-changer">
             <select>
-              <option value="none" disabled>Move to...</option>
+              <option value="none" disabled>
+                Move to...
+              </option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
               <option value="read">Read</option>
@@ -19,8 +28,8 @@ class Book extends React.Component {
         <div className="book-title">{this.props.title}</div>
         <div className="book-authors">{this.props.authors}</div>
       </div>
-    )
+    );
   }
 }
 
-export default Book
+export default Book;
